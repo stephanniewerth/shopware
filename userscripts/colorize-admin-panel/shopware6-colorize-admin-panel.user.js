a173161a91a3fb5a714ed07b2e1bf991538bc5b4
Stephan Niewerth <snw@heise.de>
2021-11-15 19:37:14 +0100

    chore: add download and update url for script

--- userscripts/colorize-admin-panel/shopware6-colorize-admin-panel.user.js
+++ userscripts/colorize-admin-panel/shopware6-colorize-admin-panel.user.js
@@ -4,6 +4,8 @@
 // @description  Colorize the administration panel to distinguish between different instances.
 // @author       Stephan Niewerth (snw@heise.de)
 // @version      0.1.0
+// @downloadURL  https://github.com/derjumpy/shopware/raw/main/userscripts/colorize-admin-panel/shopware6-colorize-admin-panel.user.js
+// @updateURL    https://github.com/derjumpy/shopware/raw/main/userscripts/colorize-admin-panel/shopware6-colorize-admin-panel.user.js
 // @grant        none
 // ==/UserScript==
 
