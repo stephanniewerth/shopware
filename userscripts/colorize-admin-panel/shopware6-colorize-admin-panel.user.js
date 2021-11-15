41963d58946eb0d6d0daf6e23a1c4f782de5a29f
Stephan Niewerth <snw@heise.de>
2021-11-15 19:44:00 +0100

    chore: fix namespace to script - second version

--- userscripts/colorize-admin-panel/shopware6-colorize-admin-panel.user.js
+++ userscripts/colorize-admin-panel/shopware6-colorize-admin-panel.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Shopware 6 - Colorize Admin Panel
-// @namespace    https://github.com/derjumpy/shopware/userscripts/colorize-admin-panel/
+// @namespace    https://github.com/derjumpy/shopware/tree/main/userscripts/colorize-admin-panel
 // @description  Colorize the administration panel to distinguish between different instances.
 // @author       Stephan Niewerth (snw@heise.de)
-// @version      1.0.1
+// @version      1.0.2
 // @downloadURL  https://github.com/derjumpy/shopware/raw/main/userscripts/colorize-admin-panel/shopware6-colorize-admin-panel.user.js
 // @updateURL    https://github.com/derjumpy/shopware/raw/main/userscripts/colorize-admin-panel/shopware6-colorize-admin-panel.user.js
 // @grant        none
